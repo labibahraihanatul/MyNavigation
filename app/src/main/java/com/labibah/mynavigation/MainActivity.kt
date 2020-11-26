267abed0acd6e5954a3aa1b42fabc38e35d5081d
3103119100@student.smktelkom-pwt.sch.id <3103119100@student.smktelkom-pwt.sch.id>
2020-11-26 11:15:57 +0700

    navigation

---
+++ app/src/main/java/com/labibah/mynavigation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.labibah.mynavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bunde
+import com.labibah.mynavigation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/labibah/mynavigation/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.labibah.mynavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.labibah.mynavigation.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
